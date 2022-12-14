8ad61681ab83347f331688e1622d35580f67eb0d
Abdullah Dinler <71527576+abdullahdinler@users.noreply.github.com>
2022-12-14 16:08:41 +0300

    Add project files.

---
+++ SQL_Verileri_Sifreleme_FormApp/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿
+namespace SQL_Verileri_Sifreleme_FormApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TxtName = new System.Windows.Forms.TextBox();
+            this.TxtMeslek = new System.Windows.Forms.TextBox();
+            this.TxtSalary = new System.Windows.Forms.TextBox();
+            this.BtnSave = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkOrchid;
+            this.panel1.Controls.Add(this.BtnSave);
+            this.panel1.Controls.Add(this.TxtSalary);
+            this.panel1.Controls.Add(this.TxtMeslek);
+            this.panel1.Controls.Add(this.TxtName);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Location = new System.Drawing.Point(24, 22);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(260, 201);
+            this.panel1.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(4, 75);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 24);
+            this.label5.TabIndex = 4;
+            // 
+            // TxtName
+            // 
+            this.TxtName.Location = new System.Drawing.Point(17, 27);
+            this.TxtName.Name = "TxtName";
+            this.TxtName.Size = new System.Drawing.Size(225, 27);
+            this.TxtName.TabIndex = 5;
+            this.TxtName.Text = "Name";
+            // 
+            // TxtMeslek
+            // 
+            this.TxtMeslek.Location = new System.Drawing.Point(17, 66);
+            this.TxtMeslek.Name = "TxtMeslek";
+            this.TxtMeslek.Size = new System.Drawing.Size(225, 27);
+            this.TxtMeslek.TabIndex = 6;
+            this.TxtMeslek.Text = "Professıon";
+            // 
+            // TxtSalary
+            // 
+            this.TxtSalary.Location = new System.Drawing.Point(17, 107);
+            this.TxtSalary.Name = "TxtSalary";
+            this.TxtSalary.Size = new System.Drawing.Size(225, 27);
+            this.TxtSalary.TabIndex = 7;
+            this.TxtSalary.Text = "Salary";
+            // 
+            // BtnSave
+            // 
+            this.BtnSave.BackColor = System.Drawing.Color.Green;
+            this.BtnSave.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BtnSave.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnSave.Location = new System.Drawing.Point(17, 141);
+            this.BtnSave.Name = "BtnSave";
+            this.BtnSave.Size = new System.Drawing.Size(225, 37);
+            this.BtnSave.TabIndex = 8;
+            this.BtnSave.Text = "Save";
+            this.BtnSave.UseVisualStyleBackColor = false;
+            this.BtnSave.Click += new System.EventHandler(this.BtnSave_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.DarkOrchid;
+            this.panel2.Controls.Add(this.dataGridView1);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(326, 22);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(585, 201);
+            this.panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(4, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 24);
+            this.label1.TabIndex = 4;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(585, 201);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Purple;
+            this.ClientSize = new System.Drawing.Size(936, 256);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Sitka Small", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Verileri Şifrele";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button BtnSave;
+        private System.Windows.Forms.TextBox TxtSalary;
+        private System.Windows.Forms.TextBox TxtMeslek;
+        private System.Windows.Forms.TextBox TxtName;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SQL_Verileri_Sifreleme_FormApp/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SQL_Verileri_Sifreleme_FormApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Sql bağlantı adresimiz.
+        private SqlConnection con =
+            new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=TestDB;Integrated Security=True");
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List(); // Veri tabanındaki veriyi listeleyen method
+        }
+
+        // Girilen veriyi şifreleyen method
+        private string Sifrele(string key)
+        {
+            byte[] keyDizi = ASCIIEncoding.ASCII.GetBytes(key);
+            string keyEncrypt = Convert.ToBase64String(keyDizi);
+            return keyEncrypt;
+        }
+
+        // Şifreli veriyi cozen method
+        private string Coz(string key)
+        {
+            byte[] keyDizi2 = Convert.FromBase64String(key);
+            string keyEncrypt2 = ASCIIEncoding.ASCII.GetString(keyDizi2);
+            return keyEncrypt2;
+        }
+
+        //  Sql'deki veriyi DataGridView altaran method
+        private void List()
+        {
+            SqlDataAdapter da = new SqlDataAdapter("select Name as [Ad Soyad], PROFESSION as Meslek, salary as [Maaş] from Personel", con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            foreach (DataRow row in dt.Rows)
+            {
+
+                row["Ad Soyad"] = Coz(row["Ad Soyad"].ToString());
+                row["Meslek"] = Coz(row["Meslek"].ToString());
+                row["Maaş"] = Coz(row["Maaş"].ToString());
+            }
+            dataGridView1.DataSource = dt;
+        }
+
+        // Girilen bilgileri şifreli halde veri tabanına kayıt yapan method
+        public void BtnSave_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand com = new SqlCommand("insert into Personel (name,PROFESSION,salary) values(@p1,@p2,@p3)", con);
+            com.Parameters.AddWithValue("@p1", Sifrele(TxtName.Text));
+            com.Parameters.AddWithValue("@p2", Sifrele(TxtMeslek.Text));
+            com.Parameters.AddWithValue("@p3", Sifrele(TxtSalary.Text));
+            com.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Veri başarılı bir şekilde kayıt edildi.", "Bilgilendirme", MessageBoxButtons.OK,
+                MessageBoxIcon.Asterisk);
+
+            List();
+        }
+
+    }
+}
